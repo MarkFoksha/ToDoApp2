92f1d5fbc956dfec2baab00e9b1f0c5498e12aa9
Mark Foksha <mark.foksha1813@gmail.com>
2023-12-04 13:45:30 +0200

    Initial Commit

---
+++ ToDoApp2UITests/ToDoApp2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoApp2UITestsLaunchTests.swift
+//  ToDoApp2UITests
+//
+//  Created by Марк Фокша on 04.12.2023.
+//
+
+import XCTest
+
+final class ToDoApp2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
