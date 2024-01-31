a3aeafbc6c1c731b3c39036ffc5103e46526a027
Mark Foksha <mark.foksha1813@gmail.com>
2024-01-31 17:34:55 +0200

    Created TaskCell and covered it with tests Created the first mock TableView Refactured the code

---
+++ ToDoApp2/View/TaskCell.swift
@@ -0,0 +1,12 @@
+//
+//  TaskCell.swift
+//  ToDoApp2
+//
+//  Created by Марк Фокша on 31.01.2024.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+ 
+}
--- ToDoApp2Tests/Controller/DataProviderTests.swift
+++ ToDoApp2Tests/Controller/DataProviderTests.swift
@@ -12,10 +12,18 @@ final class DataProviderTests: XCTestCase {
     
     var sut: DataProvider!
     var tableView: UITableView!
+    var controller: TaskListViewController!
 
     override func setUpWithError() throws {
         sut = DataProvider()
-        tableView = UITableView()
+        sut.taskManager = TaskManager()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        controller = storyboard.instantiateViewController(withIdentifier: String(describing: TaskListViewController.self)) as? TaskListViewController
+        
+        controller.loadViewIfNeeded()
+        
+        tableView = controller.tableView
         tableView.dataSource = sut
     }
 
@@ -29,15 +37,56 @@ final class DataProviderTests: XCTestCase {
     }
     
     func testNumberOfRowsInSectionZeroIsTasksCount() {
-        sut.taskManager = TaskManager()
-        
         sut.taskManager?.add(task: Task(title: "Foo"))
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
-        
         
         sut.taskManager?.add(task: Task(title: "Bar"))
         tableView.reloadData()
         
         XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2 )
+    }
+    
+    func testNumberOfRowsInSectionOneIsDoneTasksCount() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        sut.taskManager?.checkTask(at: 0)
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+        sut.taskManager?.checkTask(at: 0)
+        tableView.reloadData()
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2 )
+    }
+    
+    func testCellForRowAtIndexPathReturnsTaskCell() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        XCTAssertTrue(cell is TaskCell)
+    }
+    
+    func testCellForRowAtIndexPathDequeuesCellFromTableView() {
+        let mockTableView = MockTableView()
+        mockTableView.dataSource = sut
+        
+        mockTableView.register(TaskCell.self, forCellReuseIdentifier: String(describing: TaskCell.self))
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        mockTableView.reloadData()
+        
+        _ = mockTableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        XCTAssertTrue(mockTableView.cellIsDequeued)
+    }
+}
+
+extension DataProviderTests {
+    class MockTableView: UITableView {
+        var cellIsDequeued = false
+        
+        override func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+            cellIsDequeued = true
+            
+            return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        }
     }
 }
